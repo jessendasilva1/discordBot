b7642e8991fff5da9613d58561b922fa7e41a40e
jessen <jessendasilva1@gmail.com>
2020-07-01 18:35:52 -0400

    adding express and procfile

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var app = express();
 require("dotenv").config();
 const Discord = require("discord.js");
 const bot = new Discord.Client();
@@ -34,3 +36,14 @@ bot.on("message", (msg) => {
     }
 	
 });
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
