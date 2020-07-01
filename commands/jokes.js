5e6fa71475c17138602371ea1b38230f5b45e152
Jessen <jessendasilva1@gmail.com>
2020-07-01 13:06:37 -0400

    first commit

---
+++ commands/jokes.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'joke',
+    description: 'tells a random joke',
+    jokes: ['Evan leaves maple syrup on his window sill, thus ANTS APPEAR!', 
+            'Carter Douds Dentist goggles', 
+            'I defecate on floors',
+            'I still think im going to be a pro gamer', 
+            'I dont care bitch',
+            'Sam sucks at clone hero',
+            'I hate siege'],
+    execute(msg, args){
+        //change to be dynamic to an arbitrary list
+        const choice = Math.floor(Math.random() * 7);
+        msg.reply(`Heres a joke: ${jokes[choice]}`);
+        //msg.channel.send('pong')
+    }
+}
---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'ping',
+    description: 'Ping people who are afk',
+    execute(msg, args){
+        msg.reply('Bing Bong!');
+        //msg.channel.send('pong')
+    }
+}
